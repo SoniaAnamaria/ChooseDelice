e09b01a9a25e723ed535b19bffbe6e5d204dad6c
Sonia <tuculia_sonia@yahoo.com>
2023-03-28 14:56:43 +0300

    Added function for getting a delice

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,10 +11,9 @@ namespace ChooseDelice.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private List<string> conclusions;
-        private List<string> partialConclusions;
-        private List<string> facts;
-        private List<Rule> rules;
+        private static List<string> conclusions;
+        private static List<string> partialConclusions;
+        private static List<Rule> rules;
      
         public HomeController(ILogger<HomeController> logger)
         {
@@ -42,10 +41,12 @@ namespace ChooseDelice.Controllers
         [HttpPost]
         public ActionResult Submit(MainModel model)
         {
-            facts = model.Questions.Where(_ => _.IsChecked).Select(_ => RemoveUnderscore(_.Attribute)).ToList();
+            var facts = model.Questions.Where(_ => _.IsChecked).Select(_ => _.Attribute).ToList();
 
-            AnswerModel answer = new AnswerModel();
-            answer.Answer = "Nu exista";
+            AnswerModel answer = new()
+            {
+                Answer = GetResult(facts)
+            };
 
             return View(answer);
         }
@@ -67,20 +68,40 @@ namespace ChooseDelice.Controllers
             {
                 if (field.FieldType == typeof(int) && Convert.ToInt32(field.GetValue(obj)) == 1)
                 {
-                    var name = field.Name.Substring(field.Name.IndexOf("<") + 1, field.Name.IndexOf(">") - 1).ToLower();
-                    if (name != "id")
+                    var name = field.Name.Substring(field.Name.IndexOf("<") + 1, field.Name.IndexOf(">") - 1);
+                    if (name != "Id")
                         names.Add(name);
                 }
             }
             return names;
         }
 
-        private static string RemoveUnderscore(string s)
+        private static string GetResult(List<string> facts)
         {
-            var index = s.IndexOf("_");
-            if (index > 0)
-                return s.Remove(index, 1);
-            return s;
+            bool activated;
+            while (true)
+            {
+                activated = false;
+                foreach(var rule in rules)
+                {  
+                    if (rule.Premises.All(_ => facts.Contains(_)))
+                    {
+                        activated = true;
+                        if (conclusions.Contains(rule.Conclusion))
+                            if (facts.All(_ => rule.Premises.Contains(_)))
+                                return rule.Conclusion;
+                            else
+                                return "Nu exista";
+                        else
+                        {
+                            facts.Add(rule.Conclusion);
+                            facts.RemoveAll(_ => rule.Premises.Contains(_));
+                        }
+                    }
+                }
+                if(!activated)
+                   return "Nu exista";
+            }
         }
     }
 }
